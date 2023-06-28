04a1c3c4cb93c94fe56cbd0b1a5d7929063eb068
Artur <artur459382@gmai.com>
2023-06-28 23:40:50 +0600

    Create major classes

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -18,16 +18,7 @@ namespace Calendar.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<Calendar> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Calendar
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+       
+        
     }
 }
